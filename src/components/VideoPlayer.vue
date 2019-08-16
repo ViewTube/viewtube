331787c7d2b327e769276924b40e473995911711
Maurice Oegerli <maurice@oeger.li>
2019-08-17 01:19:02 +0200

    added play btn

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -10,7 +10,13 @@
       ref="video"
     ></video>
     <Spinner class="video-spinner" v-if="videoBuffering"></Spinner>
-    <div class="video-controls-overlay" v-bind:class="{visible: playerOverlayVisible}">
+    <div class="video-controls-overlay" v-bind:class="{ visible: playerOverlayVisible }">
+      <div class="top-control-overlay"></div>
+      <div class="center-control-overlay">
+        <div class="play-btn-container">
+          <div class="play-btn" v-bind:class="{ playing: videoPlaying }"></div>
+        </div>
+      </div>
       <div class="bottom-control-overlay">
         <div class="seekbar">
           <div class="seekbar-clickable"></div>
@@ -40,10 +46,17 @@ export default {
       loading: true,
       playerOverlayVisible: false,
       playerOverlayTimeout: undefined,
-      videoBuffering: true
+      videoBuffering: true,
+      videoPlaying: false
     }
   },
   methods: {
+    onVideoPlaying: function () {
+      this.videoPlaying = true
+    },
+    onVideoPaused: function () {
+      this.videoPlaying = false
+    },
     onVideoCanplay: function () {
       this.videoBuffering = false
       this.$refs.video.play()
@@ -82,13 +95,13 @@ export default {
   display: flex;
   position: relative;
 
-  .video{
+  .video {
     margin: auto;
     height: 100%;
     width: 100%;
   }
 
-  .video-spinner{
+  .video-spinner {
     position: absolute;
     top: 50%;
     left: 50%;
@@ -103,6 +116,53 @@ export default {
     width: 100%;
     opacity: 0;
     pointer-events: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    transition: opacity 300ms $intro-easing;
+
+    .top-control-overlay {
+    }
+
+    .center-control-overlay {
+      display: flex;
+
+      .play-btn-container {
+        .play-btn {
+          margin: auto;
+          width: 100px;
+          height: 100px;
+          background-color: #fff;
+          clip-path: polygon(
+            28% 4%,
+            18% 4%,
+            18% 96%,
+            28% 96%,
+            28% 4%,
+            72% 4%,
+            82% 4%,
+            82% 96%,
+            72% 96%,
+            18% 96%
+          );
+
+          &.playing {
+            clip-path: polygon(
+              18% 4%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              95% 50%,
+              18% 96%
+            );
+          }
+        }
+      }
+    }
 
     .bottom-control-overlay {
       .seekbar {
