a5a2233d996d6b9ab3cf3e4d006556fdff951aa8
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2019-11-19 08:33:53 +0100

    change font

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,6 +2,7 @@
   <div class="search" @scroll="$emit('scroll', $event)">
     <vue-headful :title="`${searchQuery} - ViewTube`" />
     <Spinner class="centered" v-if="loading"></Spinner>
+    <GradientBackground :color="'blue'" />
     <div class="filter-options"></div>
     <div v-if="!loading" class="search-videos-container">
       <component
