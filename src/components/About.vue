0b8da19930d54d2d4b174a9c1c78d4c5f74927ab
Maurice Oegerli <maurice@oeger.li>
2019-12-23 01:58:47 +0100

    ℹ initial about page

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about popup">
+    <div class="about-container popup-container">
+      <CloseIcon class="close-icon" @click.stop="$emit('close')" />
+      <h1>About</h1>
+    </div>
+    <div class="about-overlay popup-overlay" @click.stop="$emit('close')"></div>
+  </div>
+</template>
+
+<script>
+import CloseIcon from 'icons/Close'
+export default {
+  name: 'about',
+  components: {
+    CloseIcon
+  }
+
+}
+</script>
+
+<style>
+</style>
