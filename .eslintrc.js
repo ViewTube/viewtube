e0980f2e91da0b03f069e47bd127dc0d6274a7b7
Maurice Oegerli <maurice@oeger.li>
2020-06-21 22:25:36 +0200

    🔥 some file cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
   ],
+  plugins: [
+  ],
+  // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
+
