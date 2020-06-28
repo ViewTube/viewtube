35576e9504759c5b05d831d070a61d310409c10f
Maurice Oegerli <maurice@oeger.li>
2020-06-28 16:10:56 +0200

    🐛 fix timestamps

--- plugins/formatting.js
+++ plugins/formatting.js
@@ -3,17 +3,17 @@ export default (context, inject) => {
     getTimestampFromSeconds: (seconds) => {
       const ms = seconds * 1000
       const date = new Date(ms)
-      const timestampHours = toDoubleDigit(date.getHours() - 1)
+      const timestampHours = toDoubleDigit(date.getHours())
       const timestampMinutes = toDoubleDigit(date.getMinutes())
       const timestampSeconds = toDoubleDigit(date.getSeconds())
-      if (date.getHours() >= 0) {
+      if (date.getHours() > 0) {
         return `${timestampHours}:${timestampMinutes}:${timestampSeconds}`
       } else {
         return `${timestampMinutes}:${timestampSeconds}`
       }
 
       function toDoubleDigit(i) {
-        if (i < 10) {
+        if (i >= 0 && i < 10) {
           i = '0' + i
         }
         return i
