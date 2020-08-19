66588dafae6f6aab31e4f271d525624b5b33792f
Oegerli <maurice.oegerli@unisg.ch>
2020-08-19 17:19:03 +0200

    ✨ show image in notification

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -1,11 +1,12 @@
 self.addEventListener('push', (event) => {
   const data = event.data.json()
+  const video = data.video ? data.video : null
 
   console.log('push push push', event)
 
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: '/icon-192.png',
-    image: '/icon-192.png'
+    image: video ? video.videoThumbnails[2].url : null
   })
 })
