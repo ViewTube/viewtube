f2521adb7e73c3d8015457ac200a69b9968136ce
Oegerli <maurice.oegerli@unisg.ch>
2020-08-19 17:19:20 +0200

    🔇 remove log message

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -2,8 +2,6 @@ self.addEventListener('push', (event) => {
   const data = event.data.json()
   const video = data.video ? data.video : null
 
-  console.log('push push push', event)
-
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: '/icon-192.png',
