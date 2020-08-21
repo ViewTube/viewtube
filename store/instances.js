0e9ce4577119f481274830a31794d8f549faa507
Tim Meier <tim.meier@sfs.biz>
2020-08-21 10:31:38 +0200

    fetches instances from wiki
    cleaned up code
    
    fixed accidental duplication of instances
    
    🐛 currentInstance now chosen as first of array
    
    make dropdown more responsive

--- store/instances.js
+++ store/instances.js
@@ -1,13 +1,6 @@
 export const state = () => ({
-  currentInstance: 'https://invidious.snopyta.org',
-  instances: [
-    'https://invidious.snopyta.org',
-    'https://invidio.us',
-    'https://invidiou.sh',
-    'https://invidious.ggc-project.de',
-    'https://yewtu.be',
-    'https://invidious.toot.koeln'
-  ]
+  currentInstance: '',
+  instances: []
 })
 export const getters = {
   currentInstance(state) {
@@ -20,5 +13,35 @@ export const getters = {
 export const mutations = {
   changeInstance(state, instance) {
     state.currentInstance = instance
+  },
+  addInstance(state, instance) {
+    state.instances.push(instance)
+  },
+  clearInstances(state) {
+    state.instances.length = 0
+  }
+}
+export const actions = {
+  fetchInstances({ commit, state }) {
+    commit('clearInstances')
+    this.$axios
+      .get('https://raw.githubusercontent.com/wiki/iv-org/invidious/Invidious-Instances.md')
+      .then((response) => {
+        const fetchData = response.data.split('### Blocked:')[0]
+        const regex = /\[(?<host>[^ \]]+)\]\((?<uri>[^)]+)\)(?! - offline)/g
+        const matches = [...fetchData.matchAll(regex)]
+        for (const match of matches) {
+          if (!match[2].includes('.onion') && !match[2].includes('.i2p')) {
+            if (match[2].endsWith('/')) {
+              commit('addInstance', match[2].substring(0, match[2].length - 1))
+            } else {
+              commit('addInstance', match[2])
+            }
+          }
+        }
+        if (state.currentInstance.length === 0) {
+          commit('changeInstance', matches[0][2])
+        }
+      })
   }
 }
