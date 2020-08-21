dc5e1203125eb450d72b31e36d0dbbf9651c1c7b
Tim Meier <tim.meier@sfs.biz>
2020-08-21 13:33:04 +0200

    🐛 currentInstance now chosen as first of array

--- store/instances.js
+++ store/instances.js
@@ -1,6 +1,6 @@
 export const state = () => ({
-  currentInstance: 'https://invidious.snopyta.org',
-  instances: ['https://invidious.snopyta.org']
+  currentInstance: '',
+  instances: []
 })
 export const getters = {
   currentInstance(state) {
@@ -15,29 +15,32 @@ export const mutations = {
     state.currentInstance = instance
   },
   addInstance(state, instance) {
-    state.instances.push(instance);
+    state.instances.push(instance)
   },
-  clearInstances(state){
-    state.instances.length = 0;
+  clearInstances(state) {
+    state.instances.length = 0
   }
 }
 export const actions = {
-  fetchInstances({ commit }) {
-    commit('clearInstances');
+  fetchInstances({ commit, state }) {
+    commit('clearInstances')
     this.$axios
       .get('https://raw.githubusercontent.com/wiki/iv-org/invidious/Invidious-Instances.md')
-      .then(response => {
-        var fetchData = response.data.split('### Blocked:')[0];
-        const regex = /\[(?<host>[^ \]]+)\]\((?<uri>[^\)]+)\)(?! - offline)/g;
-        const matches = [...fetchData.matchAll(regex)];
+      .then((response) => {
+        const fetchData = response.data.split('### Blocked:')[0]
+        const regex = /\[(?<host>[^ \]]+)\]\((?<uri>[^)]+)\)(?! - offline)/g
+        const matches = [...fetchData.matchAll(regex)]
         for (const match of matches) {
           if (!match[2].includes('.onion') && !match[2].includes('.i2p')) {
             if (match[2].endsWith('/')) {
-              commit('addInstance', match[2].substring(0, match[2].length-1));
+              commit('addInstance', match[2].substring(0, match[2].length - 1))
             } else {
-              commit('addInstance', match[2]);
+              commit('addInstance', match[2])
             }
           }
+        }
+        if (state.currentInstance.length === 0) {
+          commit('changeInstance', matches[0][2])
         }
       })
   }
