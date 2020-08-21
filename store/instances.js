7e13e9121ca606e7344b0cc21a3d269119edc13c
Tim Meier <tim.meier@sfs.biz>
2020-08-21 10:40:15 +0200

    cleaned up code

--- store/instances.js
+++ store/instances.js
@@ -23,13 +23,12 @@ export const actions = {
     this.$axios
       .get('https://raw.githubusercontent.com/wiki/iv-org/invidious/Invidious-Instances.md')
       .then(response => {
-        var fetchData = response.data.split("### Blocked:")[0];
-        fetchData = fetchData.split("### Blocked:")[0];
+        var fetchData = response.data.split('### Blocked:')[0];
         const regex = /\[(?<host>[^ \]]+)\]\((?<uri>[^\)]+)\)(?! - offline)/g;
         const matches = [...fetchData.matchAll(regex)];
         for (const match of matches) {
-          if (!match[2].includes(".onion") && !match[2].includes(".i2p")) {
-            if (match[2].endsWith("/")) {
+          if (!match[2].includes('.onion') && !match[2].includes('.i2p')) {
+            if (match[2].endsWith('/')) {
               commit('addInstance', match[2].substring(0, match[2].length-1));
             } else {
               commit('addInstance', match[2]);
