c3295dc1d5b59532189f5b1bf2d87c5d8c6bc72b
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2020-08-25 16:11:15 +0200

    🐛 fix notification sending

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -1,13 +1,16 @@
 self.addEventListener('push', (event) => {
-  event.waitUntil(
-    event.data.json().then((data) => {
-      const video = data.video ? data.video : null
+  event.waitUntil(new Promise((resolve, reject) => {
+    const data = event.data.json()
+    const video = data.video ? data.video : null
 
-      return self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: '/icon-192.png',
-        image: video ? video.videoThumbnails[2].url : null
-      })
+    return self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon-192.png',
+      image: video ? video.videoThumbnails[2].url : null
+    }).then(() => {
+      resolve(true)
     })
+  })
+
   )
 })
