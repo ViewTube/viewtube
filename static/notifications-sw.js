4bdce905bc98ced8aa1ce2ff13aea78740db061c
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2020-08-25 15:40:15 +0200

    🐛 fix "this website has been updated in the background"

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -1,10 +1,11 @@
 self.addEventListener('push', (event) => {
-  const data = event.data.json()
-  const video = data.video ? data.video : null
+  return event.data.json().then((data) => {
+    const video = data.video ? data.video : null
 
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: '/icon-192.png',
-    image: video ? video.videoThumbnails[2].url : null
+    return self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon-192.png',
+      image: video ? video.videoThumbnails[2].url : null
+    })
   })
 })
