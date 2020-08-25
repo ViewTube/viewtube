055e4143bfd86cd69261a46ddf325bf67606a13c
Oegerli, Maurice <maurice.oegerli@unisg.ch>
2020-08-25 15:47:31 +0200

    🐛 fix notification

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -1,11 +1,13 @@
 self.addEventListener('push', (event) => {
-  return event.data.json().then((data) => {
-    const video = data.video ? data.video : null
+  event.waitUntil(
+    event.data.json().then((data) => {
+      const video = data.video ? data.video : null
 
-    return self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/icon-192.png',
-      image: video ? video.videoThumbnails[2].url : null
+      return self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: '/icon-192.png',
+        image: video ? video.videoThumbnails[2].url : null
+      })
     })
-  })
+  )
 })
