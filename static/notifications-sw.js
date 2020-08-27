8d23f37ed4c8aea6626636069d37d94a7328bf86
Maurice Oegerli <maurice@oeger.li>
2020-08-27 23:56:40 +0200

    ✨ notification click action

--- static/notifications-sw.js
+++ static/notifications-sw.js
@@ -13,3 +13,24 @@ self.addEventListener('push', (event) => {
   })
   )
 })
+
+self.addEventListener('notificationclick', function (event) {
+  const clickedNotification = event.notification
+  clickedNotification.close()
+
+  if (!event.action && clickedNotification.data.video) {
+    const url = `https://viewtube.io/watch?v=${clickedNotification.data.videox}`;
+    event.waitUntil(clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const client = windowClients[i]
+        if (client.url === url && 'focus' in client) {
+          return client.focus()
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow(url)
+      }
+    }))
+  }
+})
