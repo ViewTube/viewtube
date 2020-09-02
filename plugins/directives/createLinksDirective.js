26d32d68848804d506e6f2618ba128f6454f43e6
Maurice Oegerli <maurice@oeger.li>
2020-09-02 23:24:36 +0200

    🐛 fix description spacing

--- plugins/directives/createLinksDirective.js
+++ plugins/directives/createLinksDirective.js
@@ -3,9 +3,8 @@ const urlRegex = new RegExp(String.raw`(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?
 
 export default {
   inserted(el, binding) {
-    const text = el.textContent
+    const text = el.textContent.trim()
     const htmlText = text.replace(urlRegex, (match) => {
-      // console.log(match)
       return `<a href="${match}" target="_blank" rel="noreferrer">${match}</a>`
     })
     el.innerHTML = htmlText
