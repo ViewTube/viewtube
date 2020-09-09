d5da2811bcd05c1431ece8912235c2d328daad8e
mauriceoegerli <maurice@oeger.li>
2020-09-09 16:32:33 +0200

    📦 change xml parser

--- client/plugins/services/subscriptionConverter.js
+++ client/plugins/services/subscriptionConverter.js
@@ -1,29 +1,21 @@
-import xmlJs from 'xml-js';
+import X2js from 'x2js';
 
 export default {
   convertFromYoutubeOPMLToJson(opmlString) {
-    const jsonString = xmlJs.xml2js(opmlString, {
-      compact: true
-    });
+    const x2js = new X2js();
+    const jsonString = x2js.xml2js(opmlString);
+    console.log(jsonString);
 
-    const channelArray =
-      jsonString.opml.body.outline.outline;
-    const mappedChannelArray = this.mapYTChannelArray(
-      channelArray
-    );
+    const channelArray = jsonString.opml.body.outline.outline;
+    const mappedChannelArray = this.mapYTChannelArray(channelArray);
     return mappedChannelArray;
   },
 
   mapYTChannelArray(array) {
     return array.map(element => {
-      const channel = element._attributes;
-      const channelId = new URL(
-        channel.xmlUrl
-      ).searchParams.get('channel_id');
+      const channelId = new URL(element._xmlUrl).searchParams.get('channel_id');
       const channelTitle =
-        channel.title === channel.text
-          ? channel.title
-          : `${channel.title} | ${channel.text}`;
+        element._title === element._text ? element._title : `${element._title} | ${element._text}`;
       return {
         author: channelTitle,
         authorId: channelId
