4ef43e8f25e156d88cbda5eaac960c8a6ea42e1e
cyacedev <github@cyace.dev>
2020-09-24 13:48:26 +0200

    ✨ added route for page

--- client/components/Themes.vue
+++ client/components/Themes.vue
@@ -3,6 +3,15 @@
     <div class="popup-container">
       <CloseIcon class="close-icon" @click.stop="$emit('close')" />
       <h1>Themes</h1>
+      <nuxt-link
+        v-show="userAuthenticated"
+        id="themeManager"
+        v-ripple
+        v-tippy="'Manage your themes'"
+        to="/themeManager"
+        class="tooltip btn"
+        ><ThemeManagerIcon />Manage</nuxt-link
+      >
       <ThemeSelector />
     </div>
     <div class="popup-overlay" @click.stop="$emit('close')" />
@@ -12,19 +21,26 @@
 <script>
 import CloseIcon from 'vue-material-design-icons/Close';
 import ThemeSelector from '@/components/themes/ThemeSelector';
+import ThemeManagerIcon from 'vue-material-design-icons/OpenInNew';
 import '@/assets/styles/popup.scss';
 
 export default {
   name: 'Themes',
   components: {
     CloseIcon,
-    ThemeSelector
+    ThemeSelector,
+    ThemeManagerIcon
   },
   data() {
     return {
       themes: this.$store.getters['theme/themes'],
       currentTheme: this.$store.getters['theme/theme']
     };
+  },
+  computed: {
+    userAuthenticated() {
+      return this.$store.getters['user/isLoggedIn'];
+    }
   },
   methods: {
     onThemeChange(element, index) {
@@ -40,4 +56,30 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.btn-content {
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+
+  svg {
+    width: 28px;
+    height: 28px;
+  }
+}
+.btn {
+  text-decoration: none;
+  color: var(--theme-color);
+  transition: color 300ms $intro-easing;
+  margin: 0 5px;
+  display: flex;
+  user-select: none;
+  border-radius: 5px;
+  line-height: 100%;
+  text-align: center;
+  padding: 5px 10px;
+  box-sizing: border-box;
+  border: solid 2px transparent;
+}
+</style>
---
+++ client/pages/themeManager.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <GradientBackground :color="'theme'" />
+    <SectionTitle v-if="userAuthenticated" :title="'Subscriptions'" :link="'subscriptions'" />
+  </div>
+</template>
+
+<script>
+import SectionTitle from '@/components/SectionTitle.vue';
+import GradientBackground from '@/components/GradientBackground.vue';
+
+export default {
+  name: 'ThemeManager',
+  components: {
+    SectionTitle,
+    GradientBackground
+  }
+};
+</script>
