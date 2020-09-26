18641a047d387cc408a63472a029dd967770965f
Maurice Oegerli <maurice@oeger.li>
2020-09-26 16:01:24 +0200

    🐛 disable prettier in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier',
-    'prettier/vue',
     'prettier/@typescript-eslint'
   ],
   // add your custom rules here
