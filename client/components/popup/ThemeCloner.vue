9d8cb1f12e9910ce62147db5b992aea0f621f4bf
cyacedev <github@cyace.dev>
2020-09-28 15:56:48 +0200

    🚧 begun adding theme clone popup

---
+++ client/components/popup/ThemeCloner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="popup">
+    <div class="popup-container">
+      <CloseIcon class="close-icon" @click.stop="$emit('close')" />
+      <h1>Clone Theme</h1>
+    </div>
+  </div>
+</template>
+§
