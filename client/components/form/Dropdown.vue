6c781d6a3b75c3ef0215df95f1bddd53bb8cf668
cyacedev <github@cyace.dev>
2020-09-30 12:58:46 +0200

    💄 added new unmigrated dropdown component

---
+++ client/components/form/Dropdown.vue
@@ -0,0 +1,62 @@
+<template>
+  <VSelect
+    class="styled-dropdown"
+    :placeholder="placeholder"
+    :options="options"
+    :value="value"
+    :label="label"
+    :clearable="clearable"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script>
+import VSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+export default {
+  props: {
+    placeholder: String,
+    options: Array,
+    label: String,
+    value: Object,
+    clearable: Boolean
+  },
+  components: {
+    VSelect
+  }
+};
+</script>
+
+<style lang="scss">
+.styled-dropdown {
+  width: 100%;
+}
+.styled-dropdown .vs__dropdown-toggle,
+.styled-dropdown .vs__dropdown-menu {
+  background: var(--bgcolor-main);
+  border: none;
+  color: var(--theme-color);
+}
+
+.styled-dropdown .vs__selected,
+.styled-dropdown .vs__dropdown-option,
+.styled-dropdown .vs__search {
+  color: var(--theme-color);
+}
+
+.styled-dropdown .vs__search::placeholder {
+  color: var(--theme-color-translucent);
+}
+.styled-dropdown .vs__selected-options {
+  flex-wrap: nowrap;
+}
+
+.styled-dropdown .vs__clear,
+.styled-dropdown .vs__open-indicator {
+  fill: var(--theme-color);
+}
+
+.vs__dropdown-option--highlight {
+  background-color: var(--bgcolor-alt);
+}
+</style>
