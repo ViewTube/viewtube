ae0676fb87bd7fd56448713ac49e9f4b807051bd
cyacedev <github@cyace.dev>
2020-09-30 12:58:46 +0200

    💄 added new unmigrated dropdown component

--- client/components/popup/ThemeCloner.vue
+++ client/components/popup/ThemeCloner.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="popup">
     <div class="popup-container">
-      <CloseIcon class="close-icon" @click.stop="$emit('close')" />
-      <h1>Clone Theme</h1>
-      <VSelect
-        v-model="selectedTheme"
-        class="themeSelector styled-dropdown"
-        placeholder="Choose a Parent Theme"
-        :options="themes"
-        label="name"
-      />
+      <div class="themeCloner">
+        <CloseIcon class="close-icon" @click.stop="$emit('close')" />
+        <h1>Clone Theme</h1>
+        <Dropdown
+          placeholder="Choose a Parent Theme"
+          :options="themes"
+          label="name"
+          @input="keyChange"
+        />
+      </div>
     </div>
     <div class="popup-overlay" @click.stop="$emit('close')" />
   </div>
@@ -17,64 +18,37 @@
 
 <script>
 import CloseIcon from 'vue-material-design-icons/Close';
-import VSelect from 'vue-select';
-import 'vue-select/dist/vue-select.css';
+import Dropdown from '@/components/form/Dropdown';
 import '@/assets/styles/popup.scss';
 
 export default {
   name: 'ThemeCloner',
   components: {
     CloseIcon,
-    VSelect
+    Dropdown
   },
   props: {
     themes: Array
   },
   data: () => ({
-    selectedTheme: String
+    selectedTheme: String,
+    newThemeName: String
   }),
   mounted() {
     this.selectedTheme = '';
+    this.newThemeName = '';
   },
   methods: {
-    valueChange(element) {
-      this.selectedTheme = element.value;
+    keyChange(element) {
+      this.selectedTheme = element.key;
     }
   }
 };
 </script>
 
 <style lang="scss">
-.themeSelector {
-  margin: 0 !important;
-  width: 100%;
-}
-.styled-dropdown .vs__dropdown-toggle,
-.styled-dropdown .vs__dropdown-menu {
-  background: var(--bgcolor-main);
-  border: none;
-  color: var(--theme-color);
-}
-
-.styled-dropdown .vs__selected,
-.styled-dropdown .vs__dropdown-option,
-.styled-dropdown .vs__search {
-  color: var(--theme-color);
-}
-
-.styled-dropdown .vs__search::placeholder {
-  color: var(--theme-color-translucent);
-}
-.styled-dropdown .vs__selected-options {
-  flex-wrap: nowrap;
-}
-
-.styled-dropdown .vs__clear,
-.styled-dropdown .vs__open-indicator {
-  fill: var(--theme-color);
-}
-
-.vs__dropdown-option--highlight {
-  background-color: var(--bgcolor-alt);
+.themeCloner {
+  margin: 15px 20px;
+  width: calc(100% -40px);
 }
 </style>
