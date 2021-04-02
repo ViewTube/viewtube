84edc0e4c21156751256a3da220414e7f8234649
Maurice Oegerli <maurice@oeger.li>
2021-04-02 14:51:12 +0200

    ⚡️ dont run video thumbnails through proxy (#527)

--- server/core/proxy/proxy.controller.ts
+++ server/core/proxy/proxy.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Res,
-  Header
-} from '@nestjs/common';
+import { Controller, Get, Query, Res, Header } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { ProxyService } from './proxy.service';
@@ -17,8 +11,12 @@ export class ProxyController {
   @Get('image')
   @Header('Content-Type', 'image/jpg')
   @Header('Cache-Control', 'no-cache')
-  async getQuery(@Query('url') url: string, @Res() response: Response): Promise<void> {
-    const image = await this.proxyService.proxyImage(url, response);
+  async getQuery(
+    @Query('url') url: string,
+    @Query('local') local: boolean = false,
+    @Res() response: Response
+  ): Promise<void> {
+    const image = await this.proxyService.proxyImage(url, local, response);
     response.send(image);
   }
 }
