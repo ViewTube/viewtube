289bd4241a5c673a2a5435c27f28aba86a602edb
mauriceoegerli <maurice@oeger.li>
2021-07-15 18:12:14 +0200

    🔊 log process error

--- server/user/subscriptions/subscriptions.processor.ts
+++ server/user/subscriptions/subscriptions.processor.ts
@@ -1,10 +1,12 @@
-import { OnQueueActive, OnQueueCompleted, Processor } from '@nestjs/bull';
+import { OnQueueActive, OnQueueCompleted, OnQueueError, Process, Processor } from '@nestjs/bull';
 import { InjectModel } from '@nestjs/mongoose';
 import { Job } from 'bull';
 import { Model } from 'mongoose';
 import { General } from 'server/common/general.schema';
 import { ChannelBasicInfo } from 'server/core/channels/schemas/channel-basic-info.schema';
 import { VideoBasicInfo } from 'server/core/videos/schemas/video-basic-info.schema';
+import { Subscription } from './schemas/subscription.schema';
+import { runSubscriptionsJob } from './subscriptions-job.helper';
 
 @Processor('subscriptions')
 export class SubscriptionsProcessor {
@@ -17,23 +19,78 @@ export class SubscriptionsProcessor {
     private readonly GeneralModel: Model<General>
   ) {}
 
+  @Process()
+  async subscriptionsJob(job: Job<{ userSubscriptions: Array<Subscription> }>) {
+    const channelIds = job.data.userSubscriptions.reduce(
+      (val, { subscriptions }) => [...val, ...subscriptions.map(e => e.channelId)],
+      []
+    );
+    const uniqueChannelIds = [...new Set(channelIds)];
+    let subscriptionResults = null;
+    try {
+      subscriptionResults = await runSubscriptionsJob(uniqueChannelIds);
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (subscriptionResults) {
+      const channelsToUpdate = subscriptionResults.channelResultArray.map(channel => {
+        return {
+          updateOne: {
+            filter: { authorId: channel.authorId },
+            update: { $set: channel },
+            upsert: true
+          }
+        };
+      });
+
+      const videosToUpdate = subscriptionResults.videoResultArray.map(video => {
+        return {
+          updateOne: {
+            filter: { videoId: video.videoId },
+            update: { $set: video },
+            upsert: true
+          }
+        };
+      });
+
+      return {
+        channelsToUpdate,
+        videosToUpdate
+      };
+    }
+    return null;
+  }
+
+  @OnQueueError()
+  onError(error: Error) {
+    console.log(error);
+  }
+
   @OnQueueCompleted()
   async onCompleted(job: Job, result: { channelsToUpdate: any; videosToUpdate: any }) {
-    try {
-      await this.ChannelBasicInfoModel.bulkWrite(result.channelsToUpdate);
-      await this.VideoModel.bulkWrite(result.videosToUpdate);
+    if (result) {
+      try {
+        await this.ChannelBasicInfoModel.bulkWrite(result.channelsToUpdate);
+        await this.VideoModel.bulkWrite(result.videosToUpdate);
 
-      await this.GeneralModel.findOneAndUpdate(
-        { version: 1 },
-        { lastSubscriptionsRefresh: job.finishedOn },
-        { upsert: true }
-      ).exec();
-    } catch (_) {}
-    // this.sendUserNotifications(subscriptionResults.videoResultArray);
+        await this.GeneralModel.findOneAndUpdate(
+          { version: 1 },
+          { lastSubscriptionsRefresh: job.finishedOn },
+          { upsert: true }
+        ).exec();
+      } catch (error) {
+        console.log('error running job');
+        console.log(error);
+      }
+      // this.sendUserNotifications(subscriptionResults.videoResultArray);
 
-    console.log(
-      `done and dusted in ${job.finishedOn}: ${result.channelsToUpdate.length} channels, ${result.videosToUpdate.length} videos`
-    );
+      console.log(
+        `done and dusted in ${job.finishedOn}: ${result.channelsToUpdate.length} channels, ${result.videosToUpdate.length} videos`
+      );
+    } else {
+      console.log('subscriptions job failed');
+    }
   }
 
   @OnQueueActive()
