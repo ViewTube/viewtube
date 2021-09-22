09ebe772f7e3e833bc44fc92161572ef09f8f064
Maurice Oegerli <maurice@oeger.li>
2021-09-22 19:15:09 +0200

    🐛 fix deepscan issues

--- server/user/subscriptions/subscriptions.service.ts
+++ server/user/subscriptions/subscriptions.service.ts
@@ -164,10 +164,10 @@ export class SubscriptionsService {
       const userSubscriptionIds = userSubscriptions.subscriptions.map(e => e.channelId);
 
       if (typeof limit !== 'number') {
-        limit = parseInt(limit);
+        limit = parseInt(limit as any);
       }
       if (typeof start !== 'number') {
-        start = parseInt(start);
+        start = parseInt(start as any);
       }
       if (limit > 30) {
         limit = 30;
