e2388160c71f7e2b619da6e03e32c6802b0adf77
Tim Meier <github@cyace.dev>
2022-07-29 12:57:52 +0200

    Update client/components/form/PasswordChangeForm.vue
    Co-authored-by: Maurice Oegerli <maurice@oeger.li>

--- client/components/form/PasswordChangeForm.vue
+++ client/components/form/PasswordChangeForm.vue
@@ -32,7 +32,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api';
 import FormInput from '@/components/form/FormInput.vue';
 import CloseIcon from 'vue-material-design-icons/Close.vue';
