21ac4588f28db5b1b87322047c1e423e377ebc3b
Maurice Oegerli <maurice@oeger.li>
2022-08-06 18:26:32 +0200

    👌 IMPROVE: Reduce build time

--- server/webpack.config.cjs
+++ server/webpack.config.cjs
@@ -1,7 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = function (options) {
-  return {
+  const config = {
     ...options,
     entry: [options.entry],
-    output: { filename: 'main.cjs' }
+    output: { filename: 'main.cjs' },
+    plugins: [...options.plugins, new ForkTsCheckerWebpackPlugin()]
   };
+
+  config.module.rules[0].use.find(use => use.loader === 'ts-loader').options.transpileOnly = true;
+
+  return config;
 };
