f689df5e73f20526ab77de96e6541f3af8333b47
Maurice Oegerli <maurice@oeger.li>
2022-08-16 18:33:01 +0200

    🐛 FIX: Linting

--- shared/index.ts
+++ shared/index.ts
@@ -2,7 +2,6 @@ export { SponsorBlockSegmentsDto } from './dto/sponsorblock/sponsorblock-segment
 export { SponsorBlockSegmentDto } from './dto/sponsorblock/sponsorblock-segment.dto';
 export { getSecondsFromTimestamp, getApiUrl, getViewtubeDomain, isHttps } from './util';
 export { ApiErrorDto } from './dto/api/error.dto';
-import { components, external, operations, paths } from './api.schema';
+import { components } from './api.schema';
 
 export type ApiDto<T extends keyof components['schemas']> = components['schemas'][T];
-
