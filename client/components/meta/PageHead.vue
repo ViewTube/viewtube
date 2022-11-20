8e0d5681999060ac53f1c18fbb89770e374378c6
Maurice Oegerli <maurice@oeger.li>
2022-11-20 16:33:32 +0100

    👌 IMPROVE: Improve pagehead component error handling

--- client/components/meta/PageHead.vue
+++ client/components/meta/PageHead.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
-defineProps<{
-  title: string;
-  description: string;
-  image?: string;
-  video?: string;
+type ConditionalPropType = string | undefined | null;
+
+const props = defineProps<{
+  title: ConditionalPropType;
+  description: ConditionalPropType;
+  image?: ConditionalPropType;
+  video?: ConditionalPropType;
 }>();
+
+const safelyReturn = (prop: ConditionalPropType, returnValue: string | null = ''): string => {
+  if (
+    prop !== null &&
+    prop !== undefined &&
+    prop?.length > 0 &&
+    prop !== 'undefined' &&
+    prop !== 'null'
+  ) {
+    return prop;
+  }
+  return returnValue;
+};
+
+const titleString = computed(() => safelyReturn(props.title));
+
+const descriptionString = computed(() => safelyReturn(props.description));
+
+const imageString = computed(() => safelyReturn(props.image, null));
+
+const videoString = computed(() => safelyReturn(props.video, null));
 </script>
 
 <template>
   <Head>
-    <Title>{{ title }}</Title>
-    <Meta property="og:title" :content="title" />
-    <Meta name="description" :content="description" />
-    <Meta property="og:description" :content="description" />
-    <Meta property="og:title" :content="title" />
-    <Meta v-if="image" property="og:image" :content="image" />
-    <Meta v-if="video" property="og:video" :content="video" />
+    <Title>{{ titleString }}</Title>
+    <Meta property="og:title" :content="titleString" />
+    <Meta name="description" :content="descriptionString" />
+    <Meta property="og:description" :content="descriptionString" />
+    <Meta v-if="imageString" property="og:image" :content="imageString" />
+    <Meta v-if="videoString" property="og:video" :content="videoString" />
   </Head>
 </template>
