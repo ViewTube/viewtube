d4c36dd3a90e2261547a9a7f4dff8590dc857e8d
Maurice Oegerli <maurice@oeger.li>
2022-11-30 19:09:16 +0100

    🐛 FIX: Nuxt resolver path

--- server/src/nuxt/nuxt.resolver.ts
+++ server/src/nuxt/nuxt.resolver.ts
@@ -4,7 +4,8 @@ export const resolveNuxtPath = (): string => {
   const basePath = process.env.VIEWTUBE_BASE_DIR;
   let nuxtDir = path.resolve(__dirname, '../../client/.output');
   if (basePath) {
-    nuxtDir = path.resolve(basePath, '/client/.output');
+    nuxtDir = path.resolve(basePath, 'client/.output');
   }
+  console.log('nuxtDir', nuxtDir, basePath, __dirname);
   return nuxtDir;
 };
