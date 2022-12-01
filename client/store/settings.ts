0da095e531788097065d79ebf692bd51ca5e550a
Maurice Oegerli <maurice@oeger.li>
2022-12-01 15:46:02 +0100

    🐛 FIX: Store settings on server

--- client/store/settings.ts
+++ client/store/settings.ts
@@ -1,64 +1,74 @@
 import { defineStore } from 'pinia';
 import { insertSetters } from '@/utilities/storeSetters';
 import { useUserStore } from '@/store/user';
+import { Ref, UnwrapNestedRefs, UnwrapRef } from 'vue';
 
-type SegmentOption = 'skip' | 'ask' | 'none';
+export type SegmentOption = 'skip' | 'ask' | 'none';
 type ThemeVariant = 'default' | 'light' | 'dark-no-gradient' | 'black' | 'green';
 
-const state = {
-  theme: 'default' as ThemeVariant,
-  miniplayer: false,
-  chapters: true,
-  saveVideoHistory: true,
-  settingsSaving: false,
-  sponsorblockEnabled: true,
-  sponsorblockSegmentSponsor: 'skip' as SegmentOption,
-  sponsorblockSegmentIntro: 'ask' as SegmentOption,
-  sponsorblockSegmentOutro: 'ask' as SegmentOption,
-  sponsorblockSegmentInteraction: 'skip' as SegmentOption,
-  sponsorblockSegmentSelfpromo: 'skip' as SegmentOption,
-  sponsorblockSegmentMusicOfftopic: 'skip' as SegmentOption,
-  sponsorblockSegmentPreview: 'skip' as SegmentOption,
-  autoplay: false,
-  alwaysLoopVideo: false,
-  showHomeSubscriptions: true,
-  autoplayNextVideo: false,
-  audioModeDefault: false,
-  defaultVideoSpeed: 1,
-  defaultVideoQuality: '720p',
-  defaultAudioQuality: '192kb',
-  autoAdjustVideoQuality: true,
-  autoAdjustAudioQuality: true,
-  dashPlaybackEnabled: false,
-  coolSetting: false
-};
+export const useSettingsStore = defineStore('settings', () => {
+  const state = toRefs(
+    reactive({
+      theme: 'default' as ThemeVariant,
+      miniplayer: false,
+      chapters: true,
+      saveVideoHistory: true,
+      settingsSaving: false,
+      sponsorblockEnabled: true,
+      sponsorblockSegmentSponsor: 'skip' as SegmentOption,
+      sponsorblockSegmentIntro: 'ask' as SegmentOption,
+      sponsorblockSegmentOutro: 'ask' as SegmentOption,
+      sponsorblockSegmentInteraction: 'skip' as SegmentOption,
+      sponsorblockSegmentSelfpromo: 'skip' as SegmentOption,
+      sponsorblockSegmentMusicOfftopic: 'skip' as SegmentOption,
+      sponsorblockSegmentPreview: 'skip' as SegmentOption,
+      autoplay: false,
+      alwaysLoopVideo: false,
+      showHomeSubscriptions: true,
+      autoplayNextVideo: false,
+      audioModeDefault: false,
+      defaultVideoSpeed: 1,
+      defaultVideoQuality: '720p',
+      defaultAudioQuality: '192kb',
+      autoAdjustVideoQuality: true,
+      autoAdjustAudioQuality: true,
+      dashPlaybackEnabled: false,
+      coolSetting: false
+    })
+  );
 
-export const useSettingsStore = defineStore('settings', {
-  persist: true,
-  state: () => state,
-  actions: {
-    updateSettings(newSettings: typeof state) {
-      Object.keys(newSettings).forEach(key => {
-        this[key] = newSettings[key];
+  type StateType = UnwrapNestedRefs<typeof state>;
+
+  const updateSettings = (newSettings: Partial<StateType>) => {
+    Object.keys(newSettings).forEach((key: keyof Partial<StateType>) => {
+      state[key].value = newSettings[key];
+    });
+  };
+
+  const storeSettings = async () => {
+    state.settingsSaving.value = true;
+
+    const { apiUrl } = useApiUrl();
+    const userStore = useUserStore();
+
+    if (userStore.isLoggedIn) {
+      await $fetch(`${apiUrl}user/settings`, {
+        method: 'PUT',
+        credentials: 'include',
+        // Use reactive to unwrap properties
+        body: reactive(state)
       });
-    },
-    async storeSettings() {
-      this.settingsSaving = true;
+    }
+    state.settingsSaving.value = false;
+  };
 
-      const { apiUrl } = useApiUrl();
-      const userStore = useUserStore();
+  const setters = insertSetters(state, storeSettings);
 
-      if (userStore.isLoggedIn) {
-        await $fetch(`${apiUrl}user/settings`, {
-          method: 'PUT',
-          credentials: 'include',
-          body: this.$state
-        });
-      } else if (process.client && window) {
-        window.localStorage.setItem('viewtube-settings', JSON.stringify(this.$state));
-      }
-      this.settingsSaving = false;
-    },
-    ...insertSetters(state)
-  }
+  return {
+    ...state,
+    ...setters,
+    updateSettings,
+    storeSettings,
+    persist: true
+  };
 });
