fe08434f29fafd9d3d4995c9cd6ae6952f3eeada
Maurice Oegerli <maurice@oeger.li>
2022-12-15 11:52:43 +0100

    📦 NEW: Add pull effect on tab hover

--- client/components/tab/Menu.vue
+++ client/components/tab/Menu.vue
@@ -29,13 +29,42 @@ const selectedElementWidth = computed(() => {
 });
 
 const onPageLinkClick = (pageName: string) => {
+  resetPageLinkPull();
   emit('changePage', pageName);
+};
+
+const pullRight = ref(0);
+const pullLeft = ref(0);
+const onPageLinkMouseOver = (e: MouseEvent) => {
+  const target = e.target as HTMLElement;
+  const pageName = target.dataset.pageName;
+
+  const hoveredPageIndex = props.pages.findIndex(page => page.pageName === pageName);
+  const selectedPageIndex = props.pages.findIndex(page => page.pageName === props.currentPage);
+
+  resetPageLinkPull();
+
+  if (!(hoveredPageIndex === selectedPageIndex)) {
+    if (hoveredPageIndex > selectedPageIndex) {
+      pullRight.value = hoveredPageIndex - selectedPageIndex;
+    }
+    if (hoveredPageIndex < selectedPageIndex) {
+      pullLeft.value = selectedPageIndex - hoveredPageIndex;
+    }
+  }
+};
+const resetPageLinkPull = () => {
+  pullRight.value = 0;
+  pullLeft.value = 0;
 };
 </script>
 
 <template>
   <div class="tab-menu">
-    <div class="active-selector" />
+    <div
+      class="active-selector"
+      :class="{ 'pull-left': pullLeft > 0, 'pull-right': pullRight > 0 }"
+    />
     <a
       v-for="page in pages"
       :key="page.title"
@@ -47,6 +76,8 @@ const onPageLinkClick = (pageName: string) => {
         active: page.pageName === currentPage,
         'active-simple': page.pageName === currentPage && !jsEnabled
       }"
+      @mouseover="onPageLinkMouseOver"
+      @mouseleave="resetPageLinkPull"
       @click.prevent="onPageLinkClick(page.pageName)"
     >
       {{ page.title }}
@@ -57,7 +88,6 @@ const onPageLinkClick = (pageName: string) => {
 <style lang="scss" scoped>
 .tab-menu {
   display: flex;
-  gap: 10px;
   position: relative;
 
   .tab-link {
@@ -88,7 +118,18 @@ const onPageLinkClick = (pageName: string) => {
     box-shadow: $low-shadow;
     background-color: var(--bgcolor-alt-light);
     transition: left 300ms $intro-easing, width 300ms $intro-easing, height 150ms $intro-easing,
-      top 150ms $intro-easing;
+      top 150ms $intro-easing, transform 300ms $intro-easing;
+
+    &.pull-left {
+      transform: translateX(v-bind('`-${pullLeft * 3}px`'));
+      transition: left 300ms $intro-easing, width 300ms $intro-easing, height 150ms $intro-easing,
+        top 150ms $intro-easing, transform 600ms $intro-easing;
+    }
+    &.pull-right {
+      transform: translateX(v-bind('`${pullRight * 3}px`'));
+      transition: left 300ms $intro-easing, width 300ms $intro-easing, height 150ms $intro-easing,
+        top 150ms $intro-easing, transform 600ms $intro-easing;
+    }
   }
 }
 </style>
