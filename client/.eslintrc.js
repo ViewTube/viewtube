3e2bd2c38cd4def519c8edc6c43ffa74a28ddbe9
Maurice Oegerli <maurice@oeger.li>
2023-01-01 22:47:33 +0100

    🐛 Fix: Properly implement nuxt eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,44 +5,21 @@ module.exports = {
     node: true
   },
 
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    project: 'tsconfig.eslint.json',
-    parser: '@typescript-eslint/parser'
-  },
-
   extends: ['@nuxt/eslint-config', 'prettier'],
 
   rules: {
-    'import/named': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
     'no-unused-vars': 'off',
+    'vue/multi-word-component-names': 'warn',
+    'vue/no-v-html': 'off',
+    'vue/script-setup-uses-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_'
-      }
-    ],
-    'no-useless-constructor': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/attribute-hyphenation': ['off', { ignore: ['custom-prop'] }],
-    'vue/no-v-html': 'off',
-    'vue/script-setup-uses-vars': 'off',
-    'import/order': 'off',
-    'import/namespace': 'off',
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'any'
-        }
       }
     ]
   }
