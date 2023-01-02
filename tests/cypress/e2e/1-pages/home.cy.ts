9a83d6fcacbd7c0414fa6c87043ecca655291c03
Maurice Oegerli <maurice@oeger.li>
2023-01-02 04:00:13 +0100

    ✨ Add tests project with cypress e2e tests (#1553)
    * ✨ Add tests project with cypress e2e tests
    
    * 🐛 Fix: Daily tests
    
    * 👷 Add e2e tests to PR workflow
    
    * ⚡️ Pull cache on e2e image build, remove redundant docker build
    
    * 🐛 Fix: Use buildkit for e2e docker build
    
    * 🐛 Fix: Use buildkit for e2e docker build
    
    * 💚 Upload video results as artifacts
    
    * 💚 Upload video results as artifacts
    
    * 👽️ Update to the new github environment files

---
+++ tests/cypress/e2e/1-pages/home.cy.ts
@@ -0,0 +1,18 @@
+describe('Homepage Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8066');
+  });
+
+  it('homepage loads and displays logo', () => {
+    cy.get('.logo-link').should('have.attr', 'href', '/');
+  });
+
+  it('homepage loads and shows videos', () => {
+    cy.get('.section-title > .title').should('have.text', 'Popular videos');
+
+    cy.get(
+      '.home-videos-container > .video-entry:first > .video-entry-info > .video-info-text > .video-entry-title'
+    ).should('exist');
+  });
+  
+});
