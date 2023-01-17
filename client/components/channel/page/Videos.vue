a1d7acccd363768046af976bc1ef811cf7981e19
Maurice Oegerli <maurice@oeger.li>
2023-01-18 00:01:27 +0100

    🚧 Add videos to channel page

--- client/components/channel/page/Videos.vue
+++ client/components/channel/page/Videos.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import VideoEntry from '@/components/list/VideoEntry.vue';
+
 const route = useRoute();
 const channelId = computed(() => getChannelIdFromParam(route.params.id));
 
@@ -6,5 +8,24 @@ const { data } = useGetChannelVideos(channelId);
 </script>
 
 <template>
-  <div>{{ data }}</div>
+  <div class="videos">
+    <div class="videos-container">
+      <VideoEntry v-for="(video, index) in data?.items" :key="index" :video="video" />
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.videos {
+  .videos-container {
+    width: 100%;
+    max-width: $main-width;
+    margin: 0 auto;
+    padding: 0 15px;
+    box-sizing: border-box;
+    z-index: 10;
+    background-color: var(--bgcolor-main);
+    @include viewtube-grid;
+  }
+}
+</style>
