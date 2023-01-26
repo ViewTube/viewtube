819913c04d73e131cda68014d2caec61c245b5af
Maurice Oegerli <maurice@oeger.li>
2023-01-26 14:46:23 +0100

    ✨ Add featured channels section

--- client/components/channel/page/Channels.vue
+++ client/components/channel/page/Channels.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RelatedChannels from '../RelatedChannels.vue';
+
+const route = useRoute();
+
+const channelId = computed(() => getChannelIdFromParam(route.params.id));
+
+const { data, pending } = useGetChannelInfo(channelId);
+</script>
 
 <template>
-  <div>Channels</div>
+  <Spinner v-if="pending" />
+  <RelatedChannels
+    v-if="!pending && data.relatedChannels?.items?.length > 0"
+    :related-channels="{ ...data?.relatedChannels, type: 'channels' }"
+  />
+  <div v-if="!pending && data.relatedChannels?.items.length === 0" class="no-related-channels">
+    <p>This channel doesn't feature other channels</p>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.no-related-channels {
+  display: flex;
+  justify-content: center;
+  margin: 15px 0 0 0;
+  height: 90vh;
+}
+</style>
