27d33c057c83fa4d468ec86a2a1595858e4a0457
Maurice Oegerli <maurice@oeger.li>
2023-02-07 11:43:15 +0100

    🐛 Fix: switch to cache-manager-redis-store

--- server/src/cache-config.service.ts
+++ server/src/cache-config.service.ts
@@ -1,15 +1,18 @@
-import { CacheModuleOptions, CacheOptionsFactory, Injectable } from '@nestjs/common';
-import { RedisOptions } from 'ioredis';
-import { redisStore } from 'cache-manager-ioredis-yet';
+import { CacheModuleOptions, CacheOptionsFactory, CacheStore, Injectable } from '@nestjs/common';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-store';
+import { StoreConfig } from 'cache-manager';
 
 @Injectable()
 export class CacheConfigService implements CacheOptionsFactory {
   async createCacheOptions(): Promise<CacheModuleOptions> {
     if (process.env.NODE_ENV === 'production') {
-      const redisOptions: RedisOptions = {
-        host: process.env.VIEWTUBE_REDIS_HOST,
-        port: parseInt(process.env.VIEWTUBE_REDIS_PORT),
-        db: 0
+      const redisOptions: RedisClientOptions & StoreConfig = {
+        socket: {
+          host: process.env.VIEWTUBE_REDIS_HOST,
+          port: parseInt(process.env.VIEWTUBE_REDIS_PORT)
+        },
+        database: 0
       };
 
       if (process.env.VIEWTUBE_REDIS_PASSWORD) {
@@ -19,7 +22,7 @@ export class CacheConfigService implements CacheOptionsFactory {
       const store = await redisStore(redisOptions);
 
       return {
-        store,
+        store: store as unknown as CacheStore,
         max: 20000,
         ttl: 1800
       };
