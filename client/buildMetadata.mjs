f34c20d77feabb1c41cb7ad7fc2c27f91d3abd56
Maurice Oegerli <maurice@oeger.li>
2023-02-27 16:37:03 +0100

    🐛 Log buildmetadata output

--- client/buildMetadata.mjs
+++ client/buildMetadata.mjs
@@ -3,7 +3,11 @@ import { writeFileSync } from 'node:fs';
 
 const outFile = 'buildMetadata.json';
 
-exec(`git log --no-decorate --pretty=format:'%H;%h;%s -n 1`, (_, stdout) => {
+exec(`git log --no-decorate --pretty=format:'%H;%h;%s -n 1`, (error, stdout, stderr) => {
+  console.log('error', error);
+  console.log('stdout', stdout);
+  console.log('stderr', stderr);
+
   const [commit, abbreviated_commit, subject] = stdout.split(';');
   const metadata = {
     commit,
