886c8a5c3564592e3efec5aa6ef2a66e82102616
Maurice Oegerli <maurice@oeger.li>
2023-02-27 16:40:39 +0100

    🐛 Fix: Unterminated quoted string for buildmetadata

--- client/buildMetadata.mjs
+++ client/buildMetadata.mjs
@@ -3,10 +3,9 @@ import { writeFileSync } from 'node:fs';
 
 const outFile = 'buildMetadata.json';
 
-exec(`git log --no-decorate --pretty=format:'%H;%h;%s -n 1`, (error, stdout, stderr) => {
-  console.log('error', error);
-  console.log('stdout', stdout);
-  console.log('stderr', stderr);
+exec(`git log --no-decorate --pretty=format:'%H;%h;%s' -n 1`, (error, stdout, stderr) => {
+  if(error) console.error(error);
+  if(stderr) console.error(stderr);
 
   const [commit, abbreviated_commit, subject] = stdout.split(';');
   const metadata = {
