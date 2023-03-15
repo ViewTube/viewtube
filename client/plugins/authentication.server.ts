2dc074c2def7fae690f6167fb9b99673657adc31
Maurice Oegerli <maurice@oeger.li>
2023-03-16 00:23:38 +0100

    ⬆️ Upgrade packages (#1813)
    * ⬆️ Upgrade packages
    
    * 🐛 Fix type issies  for authentication plugin

--- client/plugins/authentication.server.ts
+++ client/plugins/authentication.server.ts
@@ -1,11 +1,12 @@
 import { useUserStore } from '@/store/user';
+import { Pinia } from 'pinia';
 
 /**
  * This plugin runs user authentication server-side,
  * if there is an existing authentication cookie
  */
 export default defineNuxtPlugin(async nuxtApp => {
-  const userStore = useUserStore(nuxtApp.$pinia);
+  const userStore = useUserStore(nuxtApp.$pinia as Pinia);
 
   const cookies = parseCookieString(nuxtApp.ssrContext.event.req.headers?.cookie);
 
