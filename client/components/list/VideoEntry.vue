0aaea175d7f4a11844d0a653af0faba634c14b6a
Maurice Oegerli <maurice@oeger.li>
2023-04-28 12:37:07 +0200

    🐛 Disable rich thumbnails

--- client/components/list/VideoEntry.vue
+++ client/components/list/VideoEntry.vue
@@ -238,6 +238,7 @@ const onVideoEntryClick = () => {
           <p class="video-description-overlay-text">{{ video.description }}</p>
         </div>
       </div>
+      <!-- Disabled temporarily, because they are very buggy -->
       <div v-if="false && video.richThumbnails" class="thmb-image-container rich-thumbnail">
         <div class="thmb-clip">
           <img
