d948b9a82c35d35134f55cd5084d00a49add1f54
Mo <maurice@oeger.li>
2023-05-04 18:31:04 +0200

    🐛 Manually send nuxt static files (#1924)

--- server/src/nuxt/nuxt.resolver.ts
+++ server/src/nuxt/nuxt.resolver.ts
@@ -1,4 +1,5 @@
-import path from 'path';
+import path from 'node:path';
+import { readdir, stat } from 'node:fs/promises';
 
 export const resolveNuxtPath = (): string => {
   const basePath = process.env.VIEWTUBE_BASE_DIR;
@@ -7,4 +8,26 @@ export const resolveNuxtPath = (): string => {
     nuxtDir = path.resolve(basePath, 'client/.output');
   }
   return nuxtDir;
+};
+
+export const resolveStaticFileList = async (nuxtPath: string): Promise<Array<string>> => {
+  const staticDir = path.resolve(nuxtPath, 'public');
+  const staticFileList = await walk(staticDir);
+  const staticFileListRelative = staticFileList.map(file => file.replace(staticDir, ''));
+  return staticFileListRelative;
+};
+
+const walk = async (dir: string) => {
+  const entries = await readdir(dir);
+  let ret: Array<string> = [];
+  for (const entry of entries) {
+    const fullpath = path.resolve(dir, entry);
+    const info = await stat(fullpath);
+    if (info.isDirectory()) {
+      ret = [...ret, ...(await walk(fullpath))];
+    } else {
+      ret = [...ret, fullpath];
+    }
+  }
+  return ret;
 };
