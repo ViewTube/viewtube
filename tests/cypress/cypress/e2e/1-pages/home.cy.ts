74d6314219c2b0592a2f07c42c2607c5f0cb9811
Maurice Oegerli <maurice@oeger.li>
2023-12-12 11:41:34 +0100

    ✅ Fix e2e test

--- tests/cypress/cypress/e2e/1-pages/home.cy.ts
+++ tests/cypress/cypress/e2e/1-pages/home.cy.ts
@@ -9,7 +9,7 @@ describe('Homepage Tests', () => {
   });
 
   it('homepage loads and shows videos', () => {
-    cy.get('.section-title > .title', { timeout: 10000 }).should('have.text', 'Popular videos');
+    cy.get('.section-title > .title', { timeout: 10000 }).should('have.text', 'Trending videos');
 
     cy.get(
       '.home-videos-container > .video-entry:first > .video-entry-info > .video-info-text > .video-entry-title'
