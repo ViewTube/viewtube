dc6b07948c88402a3e1bf4bac7bfa7c111fd286f
Maurice Oegerli <maurice@oeger.li>
2024-01-03 13:08:27 +0100

    🐛 Make frontend endpoint public

--- server/src/nuxt/nuxt.controller.ts
+++ server/src/nuxt/nuxt.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get, Req, Res } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { NuxtService } from './nuxt.service';
-import { BypassAuth } from 'server/auth/decorators/bypass-auth.decorator';
+import { Public } from 'server/auth/decorators/public.decorator';
 
 @Controller()
-@BypassAuth()
+@Public()
 @ApiExcludeController()
 export class NuxtController {
   constructor(private nuxtService: NuxtService) {}
