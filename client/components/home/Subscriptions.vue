52d6e9ea3171648afc298b43c337eca3d0bece27
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2024-04-29 15:28:37 +0200

    Added Show More for subscriptions

--- client/components/home/Subscriptions.vue
+++ client/components/home/Subscriptions.vue
@@ -1,21 +1,45 @@
 <script setup lang="ts">
+
+import BadgeButton from '@/components/buttons/BadgeButton.vue';
 import VideoEntry from '@/components/list/VideoEntry.vue';
 
 const { data: subscriptions, pending: subscriptionsLoading } = useGetUserSubscriptions({
-  limit: 4
+  limit: 20
 });
+
+const showMore = ref(false);
+const displayedSubscriptions = computed(() => {
+  if (subscriptions && subscriptions.value.videos) {
+    if (!showMore.value) {
+      return subscriptions.value.videos.slice(0, 4);
+    }
+    return subscriptions.value.videos;
+  }
+  return [];
+});
+const showMoreSubscriptions = (): void => {
+  showMore.value = true;
+};
 </script>
 
 <template>
   <SectionTitle :title="'Subscriptions'" :link="'subscriptions'" />
   <Spinner v-if="subscriptionsLoading" />
-  <div v-if="subscriptions?.videos?.length > 0" class="home-videos-container small">
+  <div v-if="displayedSubscriptions.length > 0" class="home-videos-container small">
     <VideoEntry
-      v-for="video in subscriptions.videos"
+      v-for="video in displayedSubscriptions"
       :key="video.videoId"
       :video="video"
       :lazy="true"
     />
+  </div>
+  <div v-if="subscriptions?.videos?.length > 0" class="home-show-more">
+    <BadgeButton
+      v-if="displayedSubscriptions.length !== subscriptions?.videos?.length"
+      :click="showMoreSubscriptions">
+      <VTIcon name="mdi:reload" />
+      <p>Show more</p>
+    </BadgeButton>
   </div>
   <div v-if="subscriptions?.videos?.length === 0" class="no-subscriptions">
     <VTIcon name="mdi:youtube-subscription" />
