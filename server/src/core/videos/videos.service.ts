0d7a73cf232fc44c3aa72c171888f3227739e3d3
moisout <maurice@oeger.li>
2025-07-09 15:06:23 +0200

    👽️ Enable sabr mode for dash manifest

--- server/src/core/videos/videos.service.ts
+++ server/src/core/videos/videos.service.ts
@@ -47,10 +47,13 @@ export class VideosService {
       let dashManifest: string | null = null;
 
       dashManifest = await videoInfo.toDash({
-        url_transformer: (url: URL) => {
-          url.searchParams.append('__host', url.host);
-          return url;
+        manifest_options: {
+          is_sabr: true
         }
+        // url_transformer: (url: URL) => {
+        //   url.searchParams.append('__host', url.host);
+        //   return url;
+        // }
       });
 
       return dashManifest;
